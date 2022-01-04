4004bf215493a113398ed00215e0132c01aa8004
mohanapranes <mohanapraneswaran@gmail.com>
2022-01-04 15:54:48 +0530

    scraping

--- scraping.py
+++ scraping.py
@@ -1,20 +1,26 @@
 from bs4 import BeautifulSoup as bs
 import requests
+import wikipedia
+def scraping(name):
+    result = wikipedia.summary(name, sentences = 3)
 
-html_text = requests.get('https://en.wikipedia.org/wiki/Adenium').text
-soup = bs(html_text,'lxml')
-name = soup.find('h1',class_='firstHeading').text
-print("Botanical name:",name)
+    html_text = requests.get('https://en.wikipedia.org/wiki/'+name).text
+    soup = bs(html_text,'lxml')
+    name = soup.find('h1',class_='firstHeading').text
 
-details = soup.find('table', class_='infobox biota')
-list = [[]]
-for tr in details.find_all('tr'):
-    tds = tr.find_all('td')
-    if(len(tds)>1):
-        temp=[]
-        temp.append(tds[0].text)
-        temp.append(tds[1].text)
-        list.append(temp)
-for i in list:
-    print(i)
-
+    details = soup.find('table', class_='infobox biota')
+    list = [[]]
+    list.append(["Botanical name:",name])
+    for tr in details.find_all('tr'):
+        tds = tr.find_all('td')
+        if(len(tds)==1):
+            temp=[]
+            temp.append(tds[0].text)
+            list.append(temp)
+        elif(len(tds)>1):
+            temp=[]
+            temp.append(tds[0].text)
+            temp.append(tds[1].text)
+            list.append(temp)
+    list.append(["summary:",result])
+    return list
